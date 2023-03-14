ebb628378a48228b10099132b793f208e9337a51
sekzhao <sekzhao@qq.com>
2023-03-14 09:55:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # 恢复首页显示
 sed -i 's/^.*mwan.htm.*/#&/' package/lean/default-settings/files/zzz-default-settings
@@ -42,30 +42,7 @@ rm -rf ./package/applications/passwall_package
 #sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
 #sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
 
-# 添加新主题
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/applications/luci-app-argon-config
-#if [ ! -d "./package/lean/luci-app-argon-config" ]; then git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config;   else cd ./package/lean/luci-app-argon-config; git stash; git stash drop; git pull; cd ..; cd ..; cd ..; fi;
 
-
-#升级smartdns版本到最新commits
-sed -i 's/1.2023.41/'"$(date +"%Y%m%d")"'/g' feeds/packages/net/smartdns/Makefile
-sed -i '/PKG_SOURCE_VERSION:=/d' feeds/packages/net/smartdns/Makefile
-sed -i "/smartdns.git/a\PKG_SOURCE_VERSION:=$(curl -s https://api.github.com/repos/pymumu/smartdns/commits | grep '"sha"' | head -1 | cut -d '"' -f 4)" feeds/packages/net/smartdns/Makefile
-#sed -i 's/60a3719ec739be2cc1e11724ac049b09a75059cb/60a3719ec739be2cc1e11724ac049b09a75059cb/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/applications/luci-app-smartdns
-git clone --depth 1 https://github.com/pymumu/smartdns package/applications/smartdns
-#mkdir -p ./package/applications/smartdns_luci
-#mkdir -p ./package/applications/smartdns
-#cp -rf ./feeds/smartdns_luci/* ./package/applications/smartdns_luci
-#cp -rf ./feeds/smartdns/* ./package/applications/smartdns
-
-#修复mosdns到V4版本
-#sed -i 's/5.1.2/4.5.3/g' feeds/packages/net/mosdns/Makefile
-#sed -i 's/cc24a30f014fa563ca2065d198fcb0bdfe2949488f1944498f815a2a73969572/2a13b1b1d862731cf946a8ceaa99523a0eb0eaae56045dd31207b61a5a6d47ae/g' feeds/packages/net/mosdns/Makefile
-
-#修复luci-app-lucky引用
-sed -i 's/^#LUCI_DEPENDS/LUCI_DEPENDS/g' feeds/lucky/luci-app-lucky/Makefile
 
 #干掉跑分程序
 sed -i 's, <%=luci.sys.exec("cat /etc/bench.log") or " "%><,<,g'  package/lean/autocore/files/x86/index.htm
